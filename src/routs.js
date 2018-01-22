e5bb0762cb59e9c4bf6c47d66c7f2ad69f04a8e5
Michael K <michael.k@testerlabs.com>
2018-01-22 12:58:30 +0200

    test

--- src/routs.js
+++ src/routs.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 import { Route, Switch, } from 'react-router-dom';
 
-import Chat from './components/Chat/Chat';
-import Programs from './components/Programs/Programs';
+
 import Home from './components/Home/Home';
-import GroupPage from './components/GroupPage/GroupPage';
 
 const Routs = () => (
     <Switch>
